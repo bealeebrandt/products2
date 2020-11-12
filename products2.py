593d75f294f80fd3781196ee48ef682d0264fe60
Kim Brandt-Madsen <bea1209@gmail.com>
2020-11-12 09:28:41 +0100

    add check if file exist products2.py

--- products2.py
+++ products2.py
@@ -1,3 +1,19 @@
+import os
+
+products = []
+if os.path.isfile('products2.csv'):
+    print('Yeah! You found it!')
+    with open('products2.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if 'Product, price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+
+else:
+    print('Sorry! We cannot find it...')
+
 products = []
 while True:
     name = input('請輸入商品名稱：')
@@ -7,9 +23,14 @@ while True:
     p = []
     p.append(name)
     p.append(price)
+    price = int(price)
     products.append([name, price])
 print(products)
 
 for p in products:
     print(p[0], '的價格是', p[1])
 
+with open('products2.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
